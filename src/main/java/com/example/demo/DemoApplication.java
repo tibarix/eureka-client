767362d38592a40218ab29af9e55972633e892db
maayachi <maayachi@sqli.com>
2019-04-07 17:34:29 +0100

    eureka client config

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RestController
+	public class HelloWorldController {
+		@GetMapping("/hello/{name}")
+		public String getHelloWorld (@PathVariable String name){
+			return "Hello World "+name;
+		}
 	}
 
 }
